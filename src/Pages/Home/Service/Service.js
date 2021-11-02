bac77536586d2bbbe82025f708d7b2b6954264b6
nmso2 <nmsubho@gmail.com>
2021-11-02 19:09:08 +0600

    added services

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-const Service = () => {
+const Service = (props) => {
+    const { name, description, img } = props.service;
     return (
-        <div>
-            <h2>This is service!</h2>
-        </div>
+        <Card sx={{ maxWidth: 345, border: 0, boxShadow: 0 }}>
+            <CardMedia
+                component="img"
+                style={{ width: 'auto', margin: '0 auto' }}
+                image={img}
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import Service from '../Service/Service';
 import { Container } from '@mui/material';
 import fluoride from '../../../images/fluoride.png';
@@ -29,10 +30,18 @@ const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
+                <Typography sx={{ fontWeight: 500, m: 2, color: 'success.main' }} gutterBottom variant="h6" component="div">
+                    OUR SERVICES
+                </Typography>
+                <Typography sx={{ fontWeight: 600, m: 5 }} gutterBottom variant="h4" component="div">
+                    Services we provide
+                </Typography>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {Array.from(Array(6)).map((_, index) => (
+                    {services.map((service, index) => (
                         <Grid item xs={4} sm={4} md={4} key={index}>
-                            <Service></Service>
+                            <Service
+                                service={service}
+                            ></Service>
                         </Grid>
                     ))}
                 </Grid>
