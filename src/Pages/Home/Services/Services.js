5e46b625dd8adeadc352800ee69c829a06ae2267
nmso2 <nmsubho@gmail.com>
2021-11-03 06:42:03 +0600

    appointment banner

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -30,11 +30,11 @@ const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
-                <Typography sx={{ fontWeight: 500, m: 2, color: 'success.main' }} gutterBottom variant="h6" component="div">
+                <Typography sx={{ fontWeight: 500, m: 2, color: '#31C0B3' }} gutterBottom variant="h6" component="div">
                     OUR SERVICES
                 </Typography>
                 <Typography sx={{ fontWeight: 600, m: 5 }} gutterBottom variant="h4" component="div">
-                    Services we provide
+                    Services We Provide
                 </Typography>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                     {services.map((service, index) => (
