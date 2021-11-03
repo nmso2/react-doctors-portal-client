c61cd946f34d5891b6d1bbb5f224f05936720ada
nmso2 <nmsubho@gmail.com>
2021-11-03 12:28:09 +0600

    change card to paper

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const Booking = ({booking}) => {
+const Booking = ({ booking }) => {
     return (
         <div>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography variant="h5" component="div" sx={{ color: '#31C0B3' }} gutterBottom>
-                        {booking.name}
-                    </Typography>
-                    <Typography variant="h3" sx={{ fontSize: 16, fontWeight: 600 }} gutterBottom>
-                        {booking.time}
-                    </Typography>
-
-                    <Typography sx={{ mb: 1.5, fontSize: 12 }} color="text.secondary">
-                        {booking.space} SPACE AVAILABLE
-                    </Typography>
-                </CardContent>
-                <Button variant="contained" style={{ backgroundColor: '#5CE7ED', marginBottom: 25}}>Book Appointment</Button>
-            </Card>
+            <Paper elevation={3}>
+                <Typography variant="h5" component="div" sx={{ color: '#31C0B3', paddingTop: '50px' }}>
+                    {booking.name}
+                </Typography>
+                <Typography variant="h6" sx={{ padding: '10px' }} gutterBottom>
+                    {booking.time}
+                </Typography>
+                <Typography variant="caption" display="block" sx={{ mb: 2 }} color="text.secondary">
+                    {booking.space} SPACE AVAILABLE
+                </Typography>
+                <Button variant="contained" style={{ backgroundColor: '#5CE7ED', marginBottom: 25 }}>Book Appointment</Button>
+            </Paper>
         </div>
     );
 };
