a84a42c16f3eb31bb2d92857bc09e22c6a68cdbf
nmso2 <nmsubho@gmail.com>
2021-11-20 06:23:22 +0600

    added doctor

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import { Button, Input, TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 const AddDoctor = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [image, setImage] = useState(null);
+    const [success, setSuccess] = useState(false);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!image) {
+            return;
+        }
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+        formData.append('image', image);
+
+        fetch('http://localhost:5000/doctors', {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    setSuccess('Doctor added successfully')
+                    console.log('Doctor added successfully')
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
         <div>
             <h2>Add Doctor</h2>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    sx={{ width: '50%', py: '5px' }}
+                    label="Name"
+                    required
+                    onChange={e => setName(e.target.value)}
+                    variant="standard" />
+                <br />
+                <TextField
+                    sx={{ width: '50%', py: '5px' }}
+                    label="Email"
+                    type='email'
+                    required
+                    onChange={e => setEmail(e.target.value)}
+                    variant="standard" />
+                <br />
+                <Input
+                    sx={{ width: '50%', py: '5px' }}
+                    accept="image/*"
+                    required
+                    onChange={e => setImage(e.target.files[0])}
+                    type="file" />
+                <br />
+                <Button variant="contained" style={{ backgroundColor: '#5CE7ED', marginTop: 15 }} type='submit'>Add Doctor</Button>
+            </form>
+            {success && <p style={{ color: 'green' }}>{success}</p>}
         </div>
     );
 };
